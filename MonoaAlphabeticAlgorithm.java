d61cef88eee6dbf74a15cfae341bf9b80e5b2bd9
Keshav Bhandari <imksav@gmail.com>
2021-01-07 16:34:04 +0545

    Added decryption also and screenshot

--- MonoaAlphabeticAlgorithm.java
+++ MonoaAlphabeticAlgorithm.java
@@ -20,10 +20,11 @@ public class MonoaAlphabeticAlgorithm {
           // functions call..................
           String encryptedText = encryption(plaintext, keys, shuffled);
           System.out.println("Encrypted Text: " + encryptedText);
-          // encryption(plaintext, keys, shuffled);
+          System.out.println("Decrypted Text: " + decryption(encryptedText, keys, shuffled));
           sc.close();
      }
 
+     // shuffling........................
      public static String shuffle(String keys) {
           List<String> letters = Arrays.asList(keys.split(""));
           Collections.shuffle(letters);
@@ -34,6 +35,33 @@ public class MonoaAlphabeticAlgorithm {
           return shuffled;
      }
 
+     // decryption
+     public static String decryption(String encryptedText, String keys, String shuffled) {
+          char decryptedText[] = new char[encryptedText.length()];
+          char ch[] = new char[encryptedText.length()];
+          char ke[] = new char[keys.length()];
+          char sh[] = new char[shuffled.length()];
+          for (int i = 0; i < encryptedText.length(); i++) {
+               ch[i] = encryptedText.charAt(i);
+          }
+          for (int j = 0; j < keys.length(); j++) {
+               ke[j] = keys.charAt(j);
+          }
+          for (int k = 0; k < shuffled.length(); k++) {
+               sh[k] = shuffled.charAt(k);
+          }
+          for (int i = 0; i < encryptedText.length(); i++) {
+               for (int j = 0; j < keys.length(); j++) {
+                    if (sh[j] == encryptedText.charAt(i)) {
+                         decryptedText[i] = ke[j];
+                         break;
+                    }
+               }
+          }
+          return (new String(decryptedText));
+     }
+
+     // encryption....................
      public static String encryption(String plaintext, String keys, String shuffled) {
           char encryptedText[] = new char[plaintext.length()];
           char ch[] = new char[plaintext.length()];
@@ -41,19 +69,13 @@ public class MonoaAlphabeticAlgorithm {
           char[] sh = new char[shuffled.length()];
           for (int i = 0; i < plaintext.length(); i++) {
                ch[i] = plaintext.charAt(i);
-               System.out.print(ch[i]);
           }
-          System.out.println();
           for (int j = 0; j < keys.length(); j++) {
                ke[j] = keys.charAt(j);
-               System.out.print(ke[j]);
           }
-          System.out.println();
           for (int k = 0; k < shuffled.length(); k++) {
                sh[k] = shuffled.charAt(k);
-               System.out.print(sh[k]);
           }
-          System.out.println();
           for (int i = 0; i < plaintext.length(); i++) {
                for (int j = 0; j < keys.length(); j++) {
                     if (ke[j] == plaintext.charAt(i)) {
